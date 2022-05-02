f22cf8db1413fd3393c0ee7600c9a37d236036de
Carson Lau <carselau@gmail.com>
2022-05-02 00:51:55 -0400

    Adding comments like a good lil programmer boi

--- day_004/rps.py
+++ day_004/rps.py
@@ -31,15 +31,21 @@ scissors = '''
 
 #Write your code below this line 👇
 
-
+# Placing the provided ASCII art into a list
 rps_list = [rock, paper, scissors]
 
+# Getting the user and computer choices
 user_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n'))
 comp_choice = random.randint(0,2)
 
+# Checking to make sure the entered value is valid
 if user_choice >= 0 and user_choice <= 2:
+
+    # Printing the user choice and computer choice ASCII art
     print(f'You selected:\n {rps_list[user_choice]}\n')
     print(f'Computer selected:\n {rps_list[comp_choice]}\n')
+
+    # Comparing choices to determine the winner
     if user_choice == comp_choice:
         print('DRAW!')
     elif user_choice == 0 and comp_choice == 1:
@@ -54,5 +60,7 @@ if user_choice >= 0 and user_choice <= 2:
         print('YOU LOSE')
     elif user_choice == 2 and comp_choice == 1:
         print('YOU WIN!')
+        
+# Handling the case where an invalid selection is made
 else:
     print('Invalid selection. Exiting game.')
