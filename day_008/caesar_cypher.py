a28611849f790b234802c36c7ad6d0337915d707
Carson Lau <calau@ra.rockwell.com>
2022-05-06 01:09:51 -0400

    Condensed encrypt and decrypt into single function

--- day_008/caesar_cypher.py
+++ day_008/caesar_cypher.py
@@ -7,47 +7,21 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# Function for the encryption process
-def encrypt(word, shift):
-
-    # Initialize empty array for letters
-    encrypted_letters = []
-    # loop through each letter in the input word
-    for letter in word:
-        # generate a new index based on letter's position in the alphabet + the shift value
-        letter_index = alphabet.index(letter) + shift
-        # if the index goes outside the range of the alphabet, subtract to come back to start of alphabet
-        if letter_index >= len(alphabet):
-            letter_index -= len(alphabet)
-        # get the shifted letter and append it to the array
-        shifted_letter = alphabet[letter_index]
-        encrypted_letters.append(shifted_letter)
-    print(''.join(encrypted_letters) + '\n')
-
-def decrypt(cipher, shift):
-
-    # Initialize empty array for letters
-    decrypted_letters = []
-    # loop through each letter in the cipher
-    for letter in cipher:
-        # generate new index based on letter's position in alphabet - the shift value
-        letter_index = alphabet.index(letter) - shift
-        # Negative indexes are a thing, so doing math to prevent index error not needed!
-        unshifted_letter = alphabet[letter_index]
-        decrypted_letters.append(unshifted_letter)
-    print(''.join(decrypted_letters) + '\n')
-
-
 # Overall caesar function
 def caesar(direction, message, shift_value):
-        # Call the encrypt/decrypt function based on direction input.
-        if direction == 'encode':
-            encrypt(message, shift_value)
-        elif direction == 'decode':
-            decrypt(message, shift_value)
-        else:
-            print('Invalid selection direction selected')
 
+    output_letters = []
+    for letter in message:
+        if direction == 'encode':
+            letter_index = alphabet.index(letter) + shift
+            # if the index goes outside the range of the alphabet, subtract to come back to start of alphabet
+            if letter_index >= len(alphabet):
+                letter_index -= len(alphabet)
+        elif direction == 'decode':
+            letter_index = alphabet.index(letter) - shift
+        output_letters.append(alphabet[letter_index])
+    
+    print(''.join(output_letters))
 
 run_program = True
 
