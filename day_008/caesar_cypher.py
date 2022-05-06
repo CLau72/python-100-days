1a956c36c2a081b4c630514a793dfca02c111b9c
Carson Lau <calau@ra.rockwell.com>
2022-05-06 01:53:58 -0400

    Allow repeated entry, and fix input cases

--- day_008/caesar_cypher.py
+++ day_008/caesar_cypher.py
@@ -7,32 +7,44 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
 # Overall caesar function
 def caesar(direction, message, shift_value):
 
     output_letters = []
+    # Account for shift numbers > 26
+    shift_value = shift_value % 26
+
     for letter in message:
-        if direction == 'encode':
-            letter_index = alphabet.index(letter) + shift
-            # if the index goes outside the range of the alphabet, subtract to come back to start of alphabet
-            if letter_index >= len(alphabet):
-                letter_index -= len(alphabet)
-        elif direction == 'decode':
-            letter_index = alphabet.index(letter) - shift
-        output_letters.append(alphabet[letter_index])
+        if letter in alphabet:
+            if direction == 'encode':
+                letter_index = alphabet.index(letter) + shift_value
+                # if the index goes outside the range of the alphabet, subtract to come back to start of alphabet
+                if letter_index >= len(alphabet):
+                    letter_index -= len(alphabet)
+            elif direction == 'decode':
+                letter_index = alphabet.index(letter) - shift_value
+            output_letters.append(alphabet[letter_index])
+        # If character isn't a letter, just append the letter
+        else:
+            output_letters.append(letter)
     
     print(''.join(output_letters))
 
-run_program = True
+# recursive function to restart the program based on prompt.
+# If user wants to restart the program, prmopt for new inputs and call the caesar function
+# after encoding/decoding, call restart again
+def restart():
+    again = input('\nRestart program? (y/n)\n').lower()
+    if again == 'y':
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        caesar(direction, text, shift)
+        restart()
 
-# Loop the program until a user states they don't want to restart
-while run_program:
-
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    caesar(direction, text, shift)
-    
-    restart = input('Would you like restart? (y/n)\n').lower()
-    if restart == 'n':
-        run_program = False
+caesar(direction, text, shift)
+restart()
