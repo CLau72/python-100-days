020caa663cd63e3d6677c2acb79f68214ecb1868
Carson Lau <calau@ra.rockwell.com>
2022-05-18 18:31:22 -0400

    Program snake movement

--- day_020/main.py
+++ day_020/main.py
@@ -6,6 +6,7 @@ screen = Screen()
 screen.setup(width=600, height=600, startx=660, starty=240)
 screen.bgcolor("black")
 screen.title("snek")
+screen.tracer()
 
 snake_segments = []
 
@@ -21,29 +22,33 @@ for snake_segment in range(3):
     x_position -= 20
     snake_segments.append(snake)
     
+snake_head = snake_segments[0]
 
 def up():
-    snake.setheading(90)
+    snake_head.setheading(90)
 
 def down():
-    snake.setheading(270)
+    snake_head.setheading(270)
 
 def left():
-    snake.setheading(180)
+    snake_head.setheading(180)
 
 def right():
-    snake.setheading(0)
+    snake_head.setheading(0)
 
 while True:
-    for segment in snake_segments:
-        segment.forward(20)
+    screen.update()
+    time.sleep(0.2)
+    for segment in range(len(snake_segments) - 1,0,-1):
+        leading_segment = snake_segments[segment-1]
+        snake_segments[segment].goto(leading_segment.position())
        
-    time.sleep(1)
-
     screen.listen()
     screen.onkey(key="w", fun = up)
     screen.onkey(key="s", fun = down)
     screen.onkey(key="a", fun = left)
     screen.onkey(key="d", fun = right)
+
+    snake_segments[0].forward(20)
 
 screen.exitonclick()
