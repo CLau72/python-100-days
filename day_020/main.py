a29eca7c542f0567ddec253c02311dbca0558d44
Carson Lau <carselau@gmail.com>
2022-05-19 00:19:18 -0400

    Refactor to use snake class

--- day_020/main.py
+++ day_020/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import random
 import time
 
@@ -8,47 +9,19 @@ screen.bgcolor("black")
 screen.title("snek")
 screen.tracer()
 
-snake_segments = []
-
-x_position = 10
-y_position = 0
-
-for snake_segment in range(3):
-    snake = Turtle("square")
-    snake.color("white")
-    snake.speed("fastest")
-    snake.penup()
-    snake.goto(x_position,y_position)
-    x_position -= 20
-    snake_segments.append(snake)
-    
-snake_head = snake_segments[0]
-
-def up():
-    snake_head.setheading(90)
-
-def down():
-    snake_head.setheading(270)
-
-def left():
-    snake_head.setheading(180)
-
-def right():
-    snake_head.setheading(0)
+snake = Snake()
 
 while True:
     screen.update()
-    time.sleep(0.2)
-    for segment in range(len(snake_segments) - 1,0,-1):
-        leading_segment = snake_segments[segment-1]
-        snake_segments[segment].goto(leading_segment.position())
+    time.sleep(0.1)
+    snake.move()
        
     screen.listen()
-    screen.onkey(key="w", fun = up)
-    screen.onkey(key="s", fun = down)
-    screen.onkey(key="a", fun = left)
-    screen.onkey(key="d", fun = right)
+    screen.onkey(key="w", fun = snake.up)
+    screen.onkey(key="s", fun = snake.down)
+    screen.onkey(key="a", fun = snake.left)
+    screen.onkey(key="d", fun = snake.right)
+    screen.onkey(key="space", fun = snake.add_segment)
 
-    snake_segments[0].forward(20)
 
 screen.exitonclick()
