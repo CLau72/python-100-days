23645a2229db813dd85c16010ae1bc031811d67a
Carson Lau <carselau@gmail.com>
2022-05-24 00:02:49 -0400

    Make squirrel data project

---
+++ day_025/squirrels/main.py
@@ -0,0 +1,18 @@
+import pandas
+
+squirrel_data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+gray_squirrel_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"])
+red_squirrel_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Red", "Black"],
+    "Count": [gray_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+
+df = pandas.DataFrame(data_dict)
+
+df.to_csv("squirrel_count.csv")
+
+
