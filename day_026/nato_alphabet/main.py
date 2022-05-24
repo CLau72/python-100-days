b4cc7297b9d045a7548ba44f2cb5893777856100
Carson Lau <carselau@gmail.com>
2022-05-24 18:21:00 -0400

    Finish NATO alphabet translator

--- day_026/nato_alphabet/main.py
+++ day_026/nato_alphabet/main.py
@@ -1,27 +1,15 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# Read alphabet CSV into a pandas DataFrame
+alphabet_df = pandas.read_csv("./nato_phonetic_alphabet.csv")
+# Create dictionary of letter:NATO translation pairs
+nato_alphabet = {row.letter:row.code for index, row in alphabet_df.iterrows()}
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+# Take in a string and give the NATO phonetic sound for each letter
+def nato_translate(input_str):
+    translation = [nato_alphabet[letter] for letter in input_str.upper()]
+    return translation
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+# Prompt for input and print list of phonetic sounds
+input_str = input("Enter value to translate: ")
+print(nato_translate(input_str))
