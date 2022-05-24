5af4e1f106d374e8e05b4b6d0c04460f32eddced
Carson Lau <carselau@gmail.com>
2022-05-24 12:00:07 -0400

    Complete practice code

---
+++ day_026/dict_comp/main2.py
@@ -0,0 +1,17 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+# print(student_data_frame)
+
+# for key,value in student_data_frame.items():
+#     print(key)
+#     print(value)
+
+for (index, row) in student_data_frame.iterrows():
+    if row.student == "Angela":
+        print(row.score)
