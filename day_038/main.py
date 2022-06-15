fa2eb23bf811e6d6636eb7c046c8cb7285da0896
Carson Lau <calau@ra.rockwell.com>
2022-06-15 18:10:16 -0400

    Finish workout tracker

--- day_038/main.py
+++ day_038/main.py
@@ -1,11 +1,14 @@
+from wsgiref import headers
 from dotenv import load_dotenv
 import os
 import requests
 from pprint import pprint
+from datetime import datetime
 
 load_dotenv()
 APP_ID = os.getenv("APP_ID")
 API_KEY = os.getenv("API_KEY")
+SHEETY_API_KEY = os.getenv("SHEETY_API_KEY")
 
 def exercise_input():
     exercise_query = input("What exercises did you do?: ")
@@ -22,10 +25,43 @@ def exercise_input():
 
     response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise", headers=headers, json=body)
     data = response.json()
-    pprint(data)
+    return data
+
+def add_workout(exercise_data={}):
+    api_endpoint = "https://api.sheety.co/de3edc42051f3aa35335b4e565509b6a/workoutTracker/workouts"
+
+    current_datetime = datetime.now()
+    date = current_datetime.strftime('%d/%m/%Y')
+    time = current_datetime.strftime('%X')
+
+    headers = {
+        "Authorization": SHEETY_API_KEY
+    }
+
+    for exercise in exercise_data["exercises"]:
+        body = {
+            "workout": {
+                "date": date,
+                "time": time,
+                "exercise": exercise["name"].title(),
+                "duration": exercise["duration_min"],
+                "calories": exercise["nf_calories"]
+            }
+        }
+        response = requests.post(url=api_endpoint, json=body, headers=headers)
+        print(response.text)
+
+def get_workouts():
+    api_endpoint = "https://api.sheety.co/de3edc42051f3aa35335b4e565509b6a/workoutTracker/workouts"
+    response = requests.get(url=api_endpoint, headers=headers)
+    data = response.json()
+    print(data)
 
 def main():
-    exercise_input()
+    # exercise_input()
+    add_workout(exercise_input())
+
+    
 
 if __name__ == "__main__":
     main()
