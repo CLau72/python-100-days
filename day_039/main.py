1a3e74ae82eb2c1298543f1d6b1c70edfaefae0d
Carson Lau <calau@ra.rockwell.com>
2022-06-17 13:44:32 -0400

    Add text notifications

--- day_039/main.py
+++ day_039/main.py
@@ -5,26 +5,36 @@ from flight_search import FlightSearch
 from flight_data import FlightData
 from notification_manager import NotificationManager
 from pprint import pprint
+import time
 
 data_manager = DataManager()
 flight_search = FlightSearch()
 
 sheet_data = data_manager.get_flight_data()
 
+def main():
+    # Fill in IATA codes
+    for i,city in enumerate(sheet_data):
+        if city["iataCode"]:
+            pass
+            # flight_data = FlightData(destination_iata=city["iataCode"])
+            # flight_search.flight_search(flight_data.search_data)
+        else:
+            city["iataCode"] = flight_search.iata_search(city)
+            data_manager.update_flight_data(i, city)
 
-# Fill in IATA codes
-for i,city in enumerate(sheet_data):
-    if city["iataCode"]:
-        pass
-        # flight_data = FlightData(destination_iata=city["iataCode"])
-        # flight_search.flight_search(flight_data.search_data)
-    else:
-        city["iataCode"] = flight_search.iata_search(city)
-        data_manager.update_flight_data(i, city)
+        flight_data = flight_search.flight_search(city["iataCode"])
+        if flight_data:
+            print(f"{city['city']}: ${flight_data.price}")
+            if int(city['lowestPrice']) > flight_data.price:
+                city['lowestPrice'] = flight_data.price
+                data_manager.update_flight_data(i, city)
+                notification_manager = NotificationManager()
+                notification_manager.flight_alert(flight_data)
+        else:
+            print(f"No direct flights to {city['city']}")
 
-    flight_data = flight_search.flight_search(city["iataCode"])
-    if flight_data:
-        print(f"{city['city']}: ${flight_data.price}")
-    else:
-        print(f"No direct flights to {city['city']}")
-        
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(3600)
--- day_039/notification_manager.py
+++ day_039/notification_manager.py
@@ -1,3 +1,35 @@
+from flight_data import FlightData
+from dotenv import load_dotenv
+import os
+from twilio.rest import Client
+
+load_dotenv()
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_TOKEN = os.getenv("TWILIO_TOKEN")
+TWILIO_NUMBER = os.getenv("TWILIO_NUMBER")
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self) -> None:
+        self.twilio_sid = TWILIO_SID
+        self.twilio_token = TWILIO_TOKEN
+        self.twilio_number = TWILIO_NUMBER
+        self.receiving_number= ""
+
+    def flight_alert(self,flight_data: FlightData):
+        client = Client(self.twilio_sid, self.twilio_token)
+
+        message_body = (f"Low price alert! Only ${flight_data.price} to fly from " 
+        f"{flight_data.origin_city}-{flight_data.origin_airport} to "
+       f"{flight_data.destination_city}-{flight_data.destination_airport} "
+        f"from {flight_data.out_date} to {flight_data.return_date}")
+
+        message = client.messages \
+                .create(
+                     body=message_body,
+                     from_=self.twilio_number,
+                     to=self.receiving_number
+                 )
+
+        print(message.sid)
